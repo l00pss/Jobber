b8b4dcb3da8bb39712362216e9e254f21dd2b526
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-04-16 22:11:53 +0400

    Update all controller

---
+++ src/main/java/com/jobber/vacancy/controller/api/jobber/JobberEmployerController.java
@@ -0,0 +1,11 @@
+package com.jobber.vacancy.controller.api.jobber;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/1.0/jobber/employer")
+@RequiredArgsConstructor
+public class JobberEmployerController {
+}
---
+++ src/main/java/com/jobber/vacancy/controller/api/panel/PanelEmployerController.java
@@ -0,0 +1,11 @@
+package com.jobber.vacancy.controller.api.panel;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/1.0/panel/employer")
+@RequiredArgsConstructor
+public class PanelEmployerController {
+}
