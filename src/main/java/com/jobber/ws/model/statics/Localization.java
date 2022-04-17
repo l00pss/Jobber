3c38e0444e8c49937a4473567b2a095bc4ae5bfc
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-04-17 17:19:33 +0400

    Update some entities

--- src/main/java/com/jobber/ws/model/statics/Localization.java
+++ src/main/java/com/jobber/ws/model/statics/Localization.java
@@ -1,5 +1,10 @@
 package com.jobber.ws.model.statics;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +12,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "LOCALIZATION")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Localization {
     @Id
     private Long id;
